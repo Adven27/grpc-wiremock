8a10c77edf871f6e94c03a6d740a5225928e9c9b
toropanov <nikolai.toropanov@betpawa.com>
2020-05-26 20:57:45 +0300

    support stream

--- src/template/java/Translator.java
+++ src/template/java/Translator.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import java.util.Arrays;
+import java.util.Map;
 @import_services@
 
 @Configuration
