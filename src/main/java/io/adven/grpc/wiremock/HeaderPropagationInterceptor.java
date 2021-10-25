6e778bf846880851f854732e9469c0b115954f95
Toropanov Nikolai <n.toropanov@alfacapital.ru>
2021-10-25 12:07:47 +0300

    closes #25

--- src/main/java/io/adven/grpc/wiremock/HeaderPropagationInterceptor.java
+++ src/main/java/io/adven/grpc/wiremock/HeaderPropagationInterceptor.java
@@ -1,5 +1,6 @@
 package io.adven.grpc.wiremock;
 
+import com.google.common.io.BaseEncoding;
 import io.grpc.Context;
 import io.grpc.Contexts;
 import io.grpc.Metadata;
@@ -10,12 +11,15 @@ import io.grpc.ServerInterceptor;
 import java.util.Map;
 
 import static io.grpc.Metadata.ASCII_STRING_MARSHALLER;
+import static io.grpc.Metadata.BINARY_BYTE_MARSHALLER;
+import static io.grpc.Metadata.BINARY_HEADER_SUFFIX;
 import static io.grpc.Metadata.Key.of;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
 
 public class HeaderPropagationInterceptor implements ServerInterceptor {
     public static final Context.Key<Map<String, String>> HEADERS = Context.key("GRPC_WIREMOCK_HEADERS");
+    private static final BaseEncoding BASE64_ENCODING_OMIT_PADDING = BaseEncoding.base64().omitPadding();
 
     @Override
     public <Req, Resp> ServerCall.Listener<Req> interceptCall(ServerCall<Req, Resp> call, final Metadata headers, ServerCallHandler<Req, Resp> next) {
@@ -28,6 +32,13 @@ public class HeaderPropagationInterceptor implements ServerInterceptor {
     }
 
     private Map<String, String> asMap(Metadata headers) {
-        return headers.keys().stream().collect(toMap(identity(), k -> headers.get(of(k, ASCII_STRING_MARSHALLER))));
+        return headers.keys().stream().collect(
+            toMap(
+                identity(),
+                k -> k.endsWith(BINARY_HEADER_SUFFIX)
+                    ? BASE64_ENCODING_OMIT_PADDING.encode(headers.get(of(k, BINARY_BYTE_MARSHALLER)))
+                    : headers.get(of(k, ASCII_STRING_MARSHALLER))
+            )
+        );
     }
 }
